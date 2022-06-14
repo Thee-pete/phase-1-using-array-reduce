9a354cd01aafded395e408cf81b1fde8030741e4
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-14 21:36:32 +0300

    commit tree 3889ba9ea128db34681eacc7e7ea44113c565a18

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-function reducer(accumalator, batteryValue){
-    return accumalator += batteryValue;
+function reducer(myAccumulator, batteryValue){
+    return myAccumulator += batteryValue;
 }
 const totalBatteries = batteryBatches.reduce(reducer);
 
